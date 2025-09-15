faef39ee004d7c9318d55cfe5be2e91e8bfbd639
Hari krishna <harisuddamalla24@gmail.com>
2025-09-16 00:00:01 +0530

    cors update

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // ---------------------------- Middleware ----------------------------
 const corsOptions = {
-    origin: ['http://localhost:3000'],
+    origin: ['https://profound-vitality-production.up.railway.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
